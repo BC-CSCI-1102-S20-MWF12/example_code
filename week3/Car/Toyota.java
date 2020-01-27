9e6ef1988ae102b173b75ab00c8ff70f00cef364
emilypx <emilypx@rit.edu>
2020-01-27 13:53:34 -0500

    updating Toyota classs

--- week3/Car/Toyota.java
+++ week3/Car/Toyota.java
@@ -1,21 +1,26 @@
-class Toyota extends Car {
+public class Toyota extends Car {
 
   public Toyota(int num_passengers, String color) {
     super(num_passengers, color);
     this.country = "Japan";
   }
 
-  void turn_on_AC() {
-    System.out.println("It's nice and cool in this car.");
+  void report_reliability() {
+    System.out.println("Consumer Reports says I am very reliable!");
+  }
+
+  public String toString() {
+    String s = color + ", " + num_passengers + ", " + speed + ", very reliable";
+    return s;
   }
 
   public static void main (String[] args) {
-    Toyota t = new Toyota(4, "blue");
-    t.go(45);
-    System.out.println(t.speed);
-
+    Toyota t = new Toyota(5, "blue");
+    t.go(65);
+    System.out.println(t);
     t.stop();
-    System.out.println(t.speed);
+    System.out.println(t);
+    t.report_reliability();
   }
 
 }
