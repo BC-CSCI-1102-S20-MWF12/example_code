edfb7867c9a11c36b1b73a247ca09378dfa3fa1f
emilypx <emilypx@rit.edu>
2020-02-26 14:58:40 -0500

    adding week 7

---
+++ week7/javadoc_example/SayHello.java
@@ -0,0 +1,20 @@
+public class SayHello {
+
+  /**
+  * This program prints out your name.
+  *
+  * @author  Emily
+  * @version 1.0
+  * @since   2019-02-27
+  */
+
+  public static void hello(String s) {
+  /**
+   * This method prints out a greeting with the provided String.
+   * @param s This is your name
+   */
+    System.out.println("Hello, " + s);
+
+  }
+
+}
